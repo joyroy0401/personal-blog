9ade67eb3e567a82e3ecfee79965ce8816e35bbd
firstwebsite <joyroy04010@gmail.com>
2021-06-14 13:56:50 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="assets/css/styles.css">
+
+        <!-- =====BOX ICONS===== -->
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+
+        <title>Professional Graphic Designer</title>
+    </head>
+    <body>
+        <!--===== HEADER =====-->
+        <header class="l-header">
+            <nav class="nav bd-grid">
+                <div>
+                    <a href="https://www.fiverr.com/joyroy0401?up_rollout=true" target="_blank" class="nav__logo">Joy Roy</a>
+                </div>
+
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a href="#home" class="nav__link active">Home</a></li>
+                        <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
+                        <li class="nav__item"><a href="#skills" class="nav__link">Skills</a></li>
+                        <li class="nav__item"><a href="#work" class="nav__link">Work</a></li>
+                        <li class="nav__item"><a href="#contact" class="nav__link">Contact</a></li>
+                        
+                    </ul>
+                </div>
+
+                <div class="nav__toggle" id="nav-toggle">
+                    <i class='bx bx-menu'></i>
+                </div>
+            </nav>
+        </header>
+
+        <main class="l-main">
+            <!--===== HOME =====-->
+            <section class="home bd-grid" id="home">
+                <div class="home__data">
+                    <h2 class="home__title">Hi,<br>I'am <span class="home__title-color">Joy ROy</span><br> Pro Graphic Designer</h2> 
+
+                    <a href="https://www.fiverr.com/joyroy0401?up_rollout=true" target="_blank" class="button">Hire Me</a>
+                </div>
+
+                <div class="home__social">
+                    <a href="https://www.linkedin.com/in/joy-roy-6ab72a15a/" target="_blank" class="home__social-icon"><i class='bx bxl-linkedin'></i></a>
+                    <a href="https://www.behance.net/joyroy" target="_blank" class="home__social-icon"><i class='bx bxl-behance' ></i></a>
+                    <a href="https://www.facebook.com/peter.roy.585112/" class="home__social-icon" target="_blank"><i class='bx bxl-facebook'></i></a>
+                    
+                    <a href="mailto: peterroy1420@gmail.com" class="home__social-icon" target="_blank"><i class='bx bx-mail-send'></i></a>
+                    
+                
+                </div>
+
+                <div class="home__img">    
+                    <img src="assets/img/joy%20roy.png" alt="">
+                </div>
+            </section>
+
+            <!--===== ABOUT =====-->
+            <section class="about section " id="about">
+                <h2 class="section-title">About</h2>
+
+                <div class="about__container bd-grid">
+                    <div class="about__img">
+                        <img src="assets/img/about.png" alt="">
+                    </div>
+                    
+                    <div>
+                        <h2 class="about__subtitle">I'am Joy Roy</h2>
+                        <p class="about__text">Hello! I am Professional of Clean Logo and Clean Flyer design with Graphic Designer .I have More Than 6 years of solid Experience on Graphic Design. I am very Efficient and Creative. </p>  
+                        
+                       <p class="about-next">An expert in- Adobe Photoshop & Adobe Illustrator: ✅ Clean Logo ✅ Modern Flyer & Poster ✅ Brochure Design ✅ Poster Design ✅ Business Card Design ✅ Catalog Design ✅ Podcast cover art and logo ✅ Retouching ✅ Background Remove ✅ Adobe Illustrator 2020 ✅ Adobe Photoshop 2020 I am delivery My Job fast with great Quality Design. Buy MY BEST SERVICE to ADVERTISE your EVENT.!!</p>      
+                    </div>                                   
+                </div>
+            </section>
+
+            <!--===== SKILLS =====-->
+            <section class="skills section" id="skills">
+                <h2 class="section-title">Skills</h2>
+
+                <div class="skills__container bd-grid">          
+                    <div>
+                        <h2 class="skills__subtitle">Profesional Skills</h2>
+                        <p class="skills__text">Freelancer graphic design and Illustrator. Illustration design, branding, logo design, flyer deisgn, poster design, custom service</p>
+                           <div class="skills__data">
+                            <div class="skills__names">
+                                <span class="skills__name">Flyer/Poster Design</span>
+                            </div>
+                            <div class="skills__bar skills__html">
+
+                            </div>
+                            <div>
+                                <span class="skills__percentage">99%</span>
+                            </div>
+                        </div>
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <span class="skills__name">Logo Design</span>
+                            </div>
+                            <div class="skills__bar skills__html">
+
+                            </div>
+                            <div>
+                                <span class="skills__percentage">98%</span>
+                            </div>
+                        </div>
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <span class="skills__name">Social Media(Facebook, Twitter,instagram, Youtube) Banner</span>
+                            </div>
+                            <div class="skills__bar skills__html">
+
+                            </div>
+                            <div>
+                                <span class="skills__percentage">99%</span>
+                            </div>
+                        </div>
+                        
+                           <div class="skills__data">
+                            <div class="skills__names">
+                            <span class="skills__name">Vector Tracing any logo and images</span>
+                            </div>
+                            <div class="skills__bar skills__html">
+
+                            </div>
+                            <div>
+                                <span class="skills__percentage">99%</span>
+                            </div>
+                        </div>
+
+                        <div class="skills__data">
+                            <div class="skills__names">
+                                <span class="skills__name">UX/UI</span>
+                            </div>
+                            <div class="skills__bar skills__ux">
+                                
+                            </div>
+                            <div>
+                                <span class="skills__percentage">85%</span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div>              
+                        <img src="assets/img/work3.jpg" alt="" class="skills__img">
+                    </div>
+                </div>
+            </section>
+
+            <!--===== WORK =====-->
+            <section class="work section" id="work">
+                <h2 class="section-title">Work</h2>
+                <div class="work__container bd-grid">
+                    <div class="work__img">
+                     <a href="https://www.fiverr.com/joyroy0401/design-awesome-flyer-brochure-and-poster-in-24-hours" target="_blank"><img  src="assets/img/fundraiser%20event%20flyer.jpg" alt=""></a>
+                    </div>
+                    <div class="work__img">
+                        <a href="https://www.fiverr.com/joyroy0401/design-awesome-flyer-brochure-and-poster-in-24-hours" target="_blank"><img src="assets/img/Mobile%20Parts%20Supply-01.png" alt=""></a>
+                    </div>
+                    <div class="work__img">
+                        <a href="https://www.fiverr.com/joyroy0401/design-awesome-flyer-brochure-and-poster-in-24-hours" target="_blank"><img src="assets/img/breezymwalker-2.png" alt=""></a>
+                    </div>
+                    <div class="work__img">
+                        <a href="https://www.fiverr.com/joyroy0401/design-awesome-flyer-brochure-and-poster-in-24-hours" target="_blank"><img src="assets/img/fundraiser%20event%20flyer.jpg" alt=""></a>
+                    </div>
+                    <div class="work__img">
+                        <a href="https://www.fiverr.com/joyroy0401/design-awesome-flyer-brochure-and-poster-in-24-hours" target="_blank"><img src="assets/img/credit-flyer.png" alt=""></a>
+                    </div>
+                    <div class="work__img">
+                        <a href="https://www.fiverr.com/joyroy0401/design-awesome-flyer-brochure-and-poster-in-24-hours" target="_blank"><img src="assets/img/truck-washing-flyer.jpg" alt=""></a>
+                    </div>
+                </div>
+            </section>
+
+            <!--===== CONTACT =====-->
+            <section class="contact section" id="contact">
+                <h2 class="section-title">Contact</h2>
+
+                <div class="contact__container bd-grid">
+                    <form action="" class="contact__form">
+                        <input type="text" placeholder="Name" class="contact__input">
+                        <input type="mail" placeholder="Email" class="contact__input">
+                        <textarea name="" id="" cols="0" rows="10" class="contact__input"></textarea>
+                        <input type="button" value="Enviar" class="contact__button button">
+                    </form>
+                </div>
+            </section>
+        </main>
+
+        <!--===== FOOTER =====-->
+        <footer class="footer">
+            <p class="footer__title">JOY ROY</p>
+            <div class="footer__social">
+                <a href="https://www.facebook.com/peter.roy.585112/" target="_blank" class="footer__icon"><i class='bx bxl-facebook' ></i></a>
+                <a href="https://twitter.com/JOYROY1420" target="_blank" class="footer__icon"><i class='bx bxl-twitter' ></i></a>
+                <a href="mailto: peterroy1420@gmail.com" target="_blank" class="footer__icon"><i class='bx bx-mail-send'></i></a>
+            </div>
+            <p>&#169; 2021 copyright all right reserved</p>
+        </footer>
+
+
+        <!--===== SCROLL REVEAL =====-->
+        <script src="assets/js/help.js"></script>
+
+        <!--===== MAIN JS =====-->
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
